7002dbd3db9174952d11b1690bd66dbac7519048
xuguodong <2995895090@qq.com>
2019-10-24 22:44:35 +0800

    bilibili全栈之巅P1-P6

---
+++ admin/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'main',
+    component: Main,
+    children: [
+      {path: '/categories/create',component: CategoryEdit}
+    ]
+  }
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>新建分类</h1>
+    <el-form label-width="120px">
+        <el-form-item label="名称">
+            <el-input v-model="model.name" @submit.native.prevent="save"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" native-type="submit">保存</el-button>
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            model: {}
+        }
+    },
+    mothods: {
+        async save () {
+            const res = await this.$http.post('categories',this.model)
+            this.$router.push('/categories/list')
+            this.$message({
+                type: 'success',
+                message: '保存成功',
+                res
+            })
+        }
+    }
+}
+</script>
